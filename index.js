d69324f6bc9c7cd99f389b7d6a9fae36af0a3c8c
noobtuber20103152 <81584747+noobtuber20103152@users.noreply.github.com>
2021-08-15 18:49:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var x = document.getElementById("likes");
+var y =  document.getElementById("fas1");
+var z =  document.getElementById("fas2");
+var v =  document.getElementById("fas3");
+var w =  document.getElementById("fas4");
+
+x.onclick = function(){
+    if(x.style.color!="blue"){
+    x.style.color = "blue";}
+    else x.style.color = "black";
+}
+y.onclick = function(){
+    if(y.style.color!="blue"){
+    y.style.color = "blue";}
+    else y.style.color = "black";
+}
+z.onclick = function(){
+    if(z.style.color!="blue"){
+    z.style.color = "blue";}
+    else z.style.color = "black";
+}
+v.onclick = function(){
+    if(v.style.color!="blue"){
+    v.style.color = "blue";}
+    else v.style.color = "black";
+}
+w.onclick = function(){
+    if(w.style.color!="blue"){
+    w.style.color = "blue";}
+    else w.style.color = "black";
+}
